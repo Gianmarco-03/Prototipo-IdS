fbf42906113ff512bbf3befeb653c6c93e1884f3
Gianmarco-03 <gianmarco.caruccio@studio.unibo.it>
2025-08-24 15:48:32 +0200

    EC definitivo

--- BackEnd/Controller/Interfaces/IHomeController.cs
+++ BackEnd/Controller/Interfaces/IHomeController.cs
@@ -7,7 +7,7 @@ namespace HomeBackend.Controllers
     {
         Task<List<Gruppo>> GetGruppi(string username);
         Task<List<Gruppo>> FindGruppi(string toSearch);
-        Task<List<EventoApprovato>> GetEventi(string username);
+        Task<List<Evento>> GetEventi(string username);
         Task<List<EventoApprovato>> FindEventi(string username, string toSearch);
         
     }
